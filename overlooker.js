04f9c63c7f857c67f62f32c8809dc872f8df0ec5
garduino <gsa@arsol.net>
2015-08-20 15:19:00 -0300

    Test Progress Bar: https://github.com/ccorcos/meteor-progress-bar

--- overlooker.js
+++ overlooker.js
@@ -1,23 +1,54 @@
+Posts = new Mongo.Collection('posts')
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  sub = Meteor.subscribe('posts')
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.main.helpers({
+    posts: function () {
+      return Posts.find()
+    },
+    progress: function() {
+      post = Posts.findOne({subId: sub.subscriptionId}, {sort:{progress:-1}})
+      console.log(post)
+      return post ? post.progress / post.total : 0
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Posts.find().count() == 0) {
+      for (var i = 0; i < 100; i++) {
+        Posts.insert({value: i})
+      };
+    }
   });
+  Meteor.publish('posts', function() {
+    var pub = this
+    var cursor = Posts.find()
+    var collectionName = cursor._cursorDescription.collectionName
+    var total = cursor.count()
+    var i = 0
+    var observerHandle = cursor.observeChanges({
+      added: function (id, fields) {
+        fields.subId = pub._subscriptionId
+        i++
+        fields.progress = i
+        fields.total = total
+        Meteor._sleepForMs(100)
+        pub.added(collectionName, id, fields);
+      },
+      changed: function (id, fields) {
+        pub.changed(collectionName, id, fields);
+      },
+      removed: function (id) {
+        pub.removed(collectionName, id);
+      }
+    })
+    pub.ready();
+    pub.onStop(function () {
+      observerHandle.stop();
+    })
+  })
 }
