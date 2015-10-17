458d68ca8c56ab5727e2b75dad2b7d41d6969255
German Arduino <garduino@gmail.com>
2015-10-17 11:17:06 -0300

    First attempts to build an example collection of posts.

--- overlooker.js
+++ overlooker.js
@@ -20,7 +20,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Posts.find().count() == 0) {
       for (var i = 0; i < 100; i++) {
-        Posts.insert({value: i})
+        Posts.insert({value: 'Article: ' + i })
       };
     }
   });
